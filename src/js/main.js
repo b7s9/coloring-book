b8b6db291926477151936a7326c2f6d32947e6b8
Mikayla Lisiy <mol25@drexel.edu>
2020-07-18 00:08:29 -0400

    💄 Added extra needed css:
    - coloringpage.html (no link)
    - removed font squirrel files
    - fixed sidenav responsiveness

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 console.log('hello world');
 
+/* side nav */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
