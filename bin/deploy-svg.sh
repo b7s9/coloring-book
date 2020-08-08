9bfe140ff943c1e10e0e9ecdde04a9fe0a157342
bschenerman <ben.schenerman@gmail.com>
2020-08-08 18:19:11 -0400

    🔧 Update svg deploy script

--- bin/deploy-svg.sh
+++ bin/deploy-svg.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 local_asset_dir='src/SVGs/'
-remote_asset_dir='/var/www/b7s9.com/html/coloring-book/test/'
+remote_asset_dir='/var/www/b7s9.com/html/coloring-book/src/SVGs/'
 
-if [ -z "$1" ]
-then
-	# run script for all assets 
-    # echo "No argument supplied"
-else
-	# run script for specific asset
-	# echo $1
-fi
+# if [ -z "$1" ]
+# then
+# 	# run script for all assets 
+#     # echo "No argument supplied"
+# else
+# 	# run script for specific asset
+# 	# echo $1
+# fi
 
 # zipping assets
-# zip "${local_asset_dir}svg.zip" "${local_asset_dir}2.svg" "${local_asset_dir}3.svg"
+if [ ! -d "temp/" ]; then
+	mkdir temp
+fi
+zip -r svg.zip $local_asset_dir
+mv svg.zip temp/svg.zip
 
 # scp to remote
 echo 'Deploying assets to remote'
 scp -r $local_asset_dir schenerfriend@b7s9.com:$remote_asset_dir
+
+# clean up
+rm -r temp
