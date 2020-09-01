29d959d547175a1323464ae8925d04d187d5487f
Ben Schenerman <earth@foolsmail.com>
2020-08-31 22:22:06 -0400

    🔧 Add gulp css

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,10 +1,14 @@
 const { series, parallel } = require('gulp');
 const { js } = require('./tasks/js.js')
-const { css } = require('./tasks/css.js')
+const { css, minifyCss } = require('./tasks/css.js')
+const del = require('del')
 
-function _clean(callback) {
+async function _clean(callback) {
+	await del(path.join(__dirname, '../build/'))
 	console.log('clean task')
 	callback()
 }
 
-exports.build = series(_clean, parallel(css, js))
+exports.css = css;
+exports.build = series(_clean, parallel(minifyCss, js))
+// exports.default = build;
--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -1,5 +1,27 @@
-function _css(callback) {
-	console.log('css task')
+const { dest, src } = require('gulp')
+const path = require('path');
+const del = require('del')
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+
+const srcDir = path.join(__dirname, '../../src/**/*.css')
+const destDir = path.join(__dirname, '../../dist/style')
+
+function _minifyCss() {
+	return src(srcDir)
+		.pipe(sourcemaps.init())
+		.pipe(concat('main.min.css'))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest(destDir))
+}
+
+exports.minifyCss = _minifyCss
+
+async function _css(callback) {
+	await del(destDir)
+	_minifyCss()
 	callback()
 }
 
