8977f62f2aca6a646226481a810737bc6918446a
Ben Schenerman <earth@foolsmail.com>
2020-09-03 18:29:31 -0400

    🔧 Update build scripts

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -6,12 +6,13 @@ const { css, minifyCss } = require('./tasks/css')
 const watch = require('./tasks/watch')
 
 async function _clean(callback) {
-	await del(path.join(__dirname, '../build/'))
-	console.log('clean task')
+	await del(path.join(__dirname, '../dist/'))
 	callback()
 }
 
 exports.css = css;
 exports.js = js;
-exports.build = series(_clean, parallel(css, js))
-exports.default = watch;
+exports.watch = watch;
+exports.clean = _clean;
+exports.build = parallel(css, js)
+exports.default = series(parallel(css, js), watch); // gulp has so many damn bugs idk why this doesn't work
