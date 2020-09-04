6006fbd2275529015f16c06c5fbde8c230bc316a
Ben Schenerman <earth@foolsmail.com>
2020-09-03 20:37:38 -0400

    🔧 Automate deployment process

--- bin/deploy-build.sh
+++ bin/deploy-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-local_dir='src/'
+echo '\r\n'
+
+local_dir='dist/'
 remote_dir='/var/www/b7s9.com/html/coloring-book/'
+
+cd $local_dir
 
 echo '\r\n'
 echo 'This script relies on Bens private key to the remote server.'
@@ -8,4 +12,4 @@ echo '\r\n'
 
 # this uses a private key file so only Ben can run it
 echo 'Deploying assets to remote'
-scp -r -C $local_dir schenerfriend@b7s9.com:$remote_dir
+scp -r -C * schenerfriend@b7s9.com:$remote_dir
