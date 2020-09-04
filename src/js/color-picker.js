cbf5738dcf4497f7ac4c33d3cc51fe0f59f9e65c
Ben Schenerman <earth@foolsmail.com>
2020-09-03 23:05:48 -0400

    ✨ Add radial color picker

--- src/js/color-picker.js
+++ src/js/color-picker.js
@@ -1,16 +1,24 @@
+// import AColorPicker from './a-color-picker';
+
 // swatch = the HTML element
 // color = internal value
 
 let selectedColor = '#00E8FC';
+const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay');
 const swatches = document.querySelectorAll('.swatch button');
 const ultraSelect = document.getElementById('ultra-selected');
+const colorPickerOpenBtn = document.getElementById('colorwheel');
+const colorPickerCloseBtn = document.getElementById('ultra-color-picker-close');
 
-// AColorPicker.from('div#ultra-color-picker')
-// 	.on('change', (picker, color) => {
-// 		ultraSelect.style.backgroundColor = color;
-// 		ultraSelect.style.borderColor = color;
-// 		selectedColor = color;
-// 	});
+AColorPicker.from('div#a-color-picker-container')
+	.on('change', (picker, color) => {
+		const hex = AColorPicker.parseColor(color, "hex");
+		console.log(picker)
+		console.log(hex)
+		colorPickerOpenBtn.style.backgroundColor = hex;
+		selectedColor = hex;
+	});
 
 for (i = 0; i < swatches.length; i++) {
 	let swatchColor = swatches[i].getAttribute('data-color')
@@ -24,3 +32,15 @@ function updateSelectedColor(color) {
 	let clickedBoxColor = color;
 	selectedColor = clickedBoxColor;
 }
+
+colorPickerOpenBtn.addEventListener('click', (e) => {
+	body.classList.add('scroll-none')
+	overlay.setAttribute('aria-hidden', 'false')
+})
+
+colorPickerCloseBtn.addEventListener('click', (e) => {
+	body.classList.remove('scroll-none')
+	setTimeout(() => {
+		overlay.setAttribute('aria-hidden', 'true')
+	}, 120);
+})
